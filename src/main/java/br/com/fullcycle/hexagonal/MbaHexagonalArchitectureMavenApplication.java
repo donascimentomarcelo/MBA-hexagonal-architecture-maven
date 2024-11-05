d2cb74cda7eaca6f4ae58c55ef1141eeef454a10
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2024-11-05 20:54:28 -0300

    first commit

---
+++ src/main/java/br/com/fullcycle/hexagonal/MbaHexagonalArchitectureMavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullcycle.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbaHexagonalArchitectureMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbaHexagonalArchitectureMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullcycle/hexagonal/MbaHexagonalArchitectureMavenApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.fullcycle.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class MbaHexagonalArchitectureMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
